5f6ff3bfd936481536c636a971bc7d9e38a366c3
jangcla <jangcla@ymail.com>
2021-06-18 16:12:10 -0700

    Finished Index and Show

--- my_cats/db/seeds.rb
+++ my_cats/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+kitty  = Cat.create(birth_date: '2012/03/21', color: 'grey', name: 'Kitty', sex: 'F', description: 'Likes to nap')
+orange  = Cat.create(birth_date: '2014/02/27', color: 'orange', name: 'Orange', sex: 'M', description: 'Hates to nap')
